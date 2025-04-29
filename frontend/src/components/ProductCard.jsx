b66d522acec78d104f8c298d5cf6038c9ee6656d
Macieira07 <macieiradavid0@gmail.com>
2025-04-29 14:06:03 +0100

    correcao de erros

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,22 @@
+import { Box, Image, Heading } from '@chakra-ui/react';
+
+const ProductCard = ({ product }) => {
+    return (
+        <Box
+            shadow='lg'
+            rounded='lg'
+            overflow='hidden'
+            transition='all 0.3s'
+            _hover={{ transform: "translateY(-5px)", shadow: "xl" }}
+        >
+            <Image src={product.image} alt={product.name} h={48} w="full" objectFit='cover' />
+
+            <Box p={4}>
+                <Heading as='h3' size="md" mb={2}>
+                    {product.name}
+                </Heading>
+            </Box>
+        </Box>
+    )
+};
+export default ProductCard;
